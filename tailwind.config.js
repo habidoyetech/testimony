f4e4a6eec09617820c84eba23463999125e0361a
Habidoye97 <abidoyeabiodun97@gmail.com>
2023-05-30 23:05:59 +0100

    testimony code

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    clipPath: {
+      triangle: 'polygon(50% 0%, 34% 30%, 66% 31%);',
+      card: 'circle(64.3% at 50% 50%);'
+
+    },
+    extend: {
+      colors: {
+        'testimony-background-primary': 'rgb(241,240,238)',
+        'font-color-light': 'rgba(22,22,22,.65)',
+        'font-color': '#161616',
+        'border-color': '#f2f6ff',
+      },
+      boxShadow: {
+        'low': '0 10px 20px rgba(0, 0, 0, .04), 0 2px 6px rgba(0, 0, 0, .04)'
+      }
+    },
+  },
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
+}
+
